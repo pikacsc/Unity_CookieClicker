28d60ceedebe2ef3d9b9e9e437d323d1834f8aab
JasonChoi <ccc11203@gmail.com>
2019-01-22 23:04:22 +0900

    [ Back to game, on main menu scripts ]

---
+++ Assets/Scripts/InGameMenu.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InGameMenu : MonoBehaviour {
+
+    
+    public GameObject m_GameAreaObject;
+    public GameObject m_MenuObject;
+
+
+    
+    public void BackToGame()
+    {
+        m_GameAreaObject.SetActive(true);
+        m_MenuObject.SetActive(false);
+    }
+
+    public void OnMenu()
+    {
+        m_GameAreaObject.SetActive(false);
+        m_MenuObject.SetActive(true);
+    }
+
+}
--- Assets/Scripts/MainMenuOptions.cs
+++ Assets/Scripts/MainMenuOptions.cs
@@ -7,8 +7,12 @@ public class MainMenuOptions : MonoBehaviour {
 
     public static bool m_IsLoading = false;
 
+    public GameObject m_MainMenuObject;
+    public GameObject m_OptionMenuObject;
+
+    
     public void NewGame()
-    {
+    { 
         SceneManager.LoadScene(1);
     }
 
@@ -17,5 +21,18 @@ public class MainMenuOptions : MonoBehaviour {
         m_IsLoading = true;
         SceneManager.LoadScene(1);
     }
-	
+
+    public void Option()
+    {
+        m_OptionMenuObject.SetActive(true);
+        m_MainMenuObject.SetActive(false);
+    }
+
+    public void BackToMainMenu()
+    {
+        m_OptionMenuObject.SetActive(false);
+        m_MainMenuObject.SetActive(true);
+    }
+
+
 }
